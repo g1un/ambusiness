3b08fa833c9d30b93593b1302750f2265dd8f40c
I.Smal <serbov@pochta.ru>
2016-05-10 20:57:20 +0500

    AmRest_Business_Solutions_base, AmRest_Business_Solutions_trainings_unit, AmRest_Business_business cases

--- app/js/script.js
+++ app/js/script.js
@@ -21,13 +21,23 @@ $(document).ready(function(){
 	/*end of image in select for feedback block*/
 
 	/*slick&owl-carousel*/
-	if($('.jq-slider ul').length){
-		$('.jq-slider ul').owlCarousel({
-			items: 4,
-			margin: 5,
-			loop: false,
-			nav: true,
-			navText: ['<div class="company-about__experts-arrow-left jq-slider__left"></div>', '<div class="company-about__experts-arrow-right jq-slider__right"></div>']
+	//if($('.jq-slider ul').length){
+	//	$('.jq-slider ul').owlCarousel({
+	//		items: 4,
+	//		margin: 5,
+	//		loop: false,
+	//		nav: true,
+	//		navText: ['<div class="company-about__experts-arrow-left jq-slider__left"></div>', '<div class="company-about__experts-arrow-right jq-slider__right"></div>']
+	//	});
+	//}
+
+	if($('.company-about__experts-slider ul').length){
+		$('.company-about__experts-slider ul').slick({
+			prevArrow: $('.company-about__experts-arrow-left'),
+			nextArrow: $('.company-about__experts-arrow-right'),
+			adaptiveHeight: true,
+			infinite: false,
+			slidesToShow: 4
 		});
 	}
 
@@ -57,34 +67,70 @@ $(document).ready(function(){
 	}
 	/*end of training-page__slider*/
 
+	/*trainings-unit__slider*/
+	if($('.trainings-unit__slider-list').length){
+		$('.trainings-unit__slider-list').slick({
+			autoplay: true,
+			arrows: false,
+			autoplaySpeed: 5000,
+			cssEase: 'ease-in-out',
+			dots: true,
+			adaptiveHeight: true,
+			dotsClass: 'trainings-unit__slider-dots'
+		});
+	}
+	/*end of trainings-unit__slider*/
+
+	/*.edu-page__articles*/
+	if($('.edu-page__articles ul').length){
+		$('.edu-page__articles ul').slick({
+			prevArrow: $('.edu-page__articles-left'),
+			nextArrow: $('.edu-page__articles-right'),
+			adaptiveHeight: true,
+			slidesToShow: 3
+		});
+	}
+	/*end of .edu-page__articles*/
+
+	/*.edu-page__books-list*/
+	if($('.edu-page__books-list ul').length){
+		$('.edu-page__books-list ul').slick({
+			prevArrow: $('.edu-page__books-list-left'),
+			nextArrow: $('.edu-page__books-list-right'),
+			adaptiveHeight: true,
+			slidesToShow: 5
+		});
+	}
+	/*end of .edu-page__books-list*/
+
 	/*end of slick&owl-carousel*/
 
 	if($('.company-about__experts-slider').length){
 
 		/*company-about__experts-arrow height*/
-		var slide = $('.company-about__experts-slider li');
-
-		function sliderHeight(){
-			var minHeight = 9999;
-			var i = 0;
-
-			slide.each(function(){
-				if($(this).outerHeight() < minHeight){
-					minHeight = $(this).outerHeight();
-				}
-				$('.company-about__experts-arrow-left, .company-about__experts-arrow-right').css('height', minHeight);
-			});
-		}
-
-		setTimeout(function () {
-			sliderHeight();
-		}, 50);
-
-		$(window).resize(function(){
-			setTimeout(function () {
-				sliderHeight();
-			}, 50);
-		});
+		//var slide = $('.company-about__experts-slider li');
+		//
+		//function sliderHeight(){
+		//	var minHeight = 9999;
+		//	var i = 0;
+		//
+		//	slide.each(function(){
+		//		if($(this).outerHeight() < minHeight){
+		//			minHeight = $(this).outerHeight();
+		//		}
+		//		$('.company-about__experts-arrow-left, .company-about__experts-arrow-right').css('height', minHeight);
+		//	});
+		//}
+		//
+		//setTimeout(function () {
+		//	sliderHeight();
+		//}, 50);
+		//
+		//$(window).resize(function(){
+		//	setTimeout(function () {
+		//		sliderHeight();
+		//	}, 50);
+		//});
 		/*end of company-about__experts-arrow height*/
 
 		/*text characters limitation in company-about__experts-slider*/
@@ -139,7 +185,18 @@ $(document).ready(function(){
 			$(this).addClass('active');
 
 			showHide();
-			sliderHeight();
+			//sliderHeight();
+
+			$('.company-about__experts-slider ul').slick('unslick');
+
+			$('.company-about__experts-slider ul').slick({
+				prevArrow: $('.company-about__experts-arrow-left'),
+				nextArrow: $('.company-about__experts-arrow-right'),
+				adaptiveHeight: true,
+				infinite: false,
+				slidesToShow: 4
+			});
+
 		});
 	}
 	/*end of company-about__nav*/
